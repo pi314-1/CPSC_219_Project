4cb7c154835d835a2af152de96b0c720169885b7
erinfouad <95101237+erinfouad@users.noreply.github.com>
2021-12-13 14:45:12 -0700

    added loader in Main, imported required packages

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -11,6 +14,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
+			
+			FXMLLoader loader = new FXMLLoader();
+			root = (BorderPane)loader.load(new 
+					FileInputStream("src/application/CalendarInterface.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module CPSC_219_ProjectInterface {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
