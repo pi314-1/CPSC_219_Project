7f8641413a3e46a28a652585814200ea88063a80
pi314-1 <95115278+pi314-1@users.noreply.github.com>
2021-12-14 11:22:14 -0700

    updates to the controller and fmxl file, now using two dropdowns for the day and time

--- src/application/CalendarController.java
+++ src/application/CalendarController.java
@@ -7,14 +7,16 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Tab;
+//import jdk.internal.jimage.ImageReader.Node;
 
-public class CalendarController {
+public class CalendarController<GridPane> {
+
 
     @FXML
-    private ChoiceBox<String> mondayChoiceBox;
-
+    private ChoiceBox<String> dayChoiceBox;
+    
     @FXML
-    private ChoiceBox<String> sundayChoiceBox;
+    private ChoiceBox<String> timeChoiceBox;
 
     @FXML
     private Tab displayEventTab;
@@ -23,25 +25,49 @@ public class CalendarController {
     private Tab addEventTab;
 
     @FXML
+    private GridPane taskGrid;
+    
+    @FXML
     private void initialize() {
     	
-    // This initializes the dropdown menus to display the time of day
-    String[] times = {"8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"};
-    sundayChoiceBox.setItems(FXCollections.observableArrayList(times));
-    mondayChoiceBox.setItems(FXCollections.observableArrayList(times));
+    // This initializes the dropdown menus
+    String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    String[] hours = {"8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"};
+    dayChoiceBox.setItems(FXCollections.observableArrayList(daysOfWeek));
+    timeChoiceBox.setItems(FXCollections.observableArrayList(hours));
   
-    
-    sundayChoiceBox.getSelectionModel().selectedIndexProperty().addListener( 
-		     new ChangeListener<Number>() { 
-		          @Override 
-		          public void changed(ObservableValue observable, Number oldValue, Number newValue) { 
-		               int index = newValue.intValue();  
-		               if (index >= 0) {
-		            	   //Event newEvent = new Event();
-		               }
-		                    
-		          }
-		     }
-		);
+    interactChoiceBox(dayChoiceBox);
+    interactChoiceBox(timeChoiceBox);
+  
 }
+    
+    /**
+     * This method allows the user to interact with the choiceboxes
+     * @param aChoiceBox
+     */
+    private void interactChoiceBox(ChoiceBox<String> aChoiceBox) {
+    	aChoiceBox.getSelectionModel().selectedIndexProperty().addListener( 
+   		     new ChangeListener<Number>() { 
+   		          @Override 
+   		          public void changed(ObservableValue observable, Number oldValue, Number newValue) { 
+   		               int index = newValue.intValue();  
+   		               if (index >= 0) {
+   		            	
+   		            	   //Event newEvent = new Event();
+   		               }
+   		                    
+   		          }
+   		     }
+   		);
+    	
+    }
+    
+    public <Node> void clickGrid(javafx.scene.input.MouseEvent event) {
+        Node source = (Node)event.getSource() ;
+//        Integer colIndex = taskGrid.getColumnIndex(source);
+//        Integer rowIndex = taskGrid.getRowIndex(source);
+//        System.out.println("Mouse clicked cell: " + colIndex + "And: " + rowIndex);
+        System.out.println("Mouse clicked cell: ");
+
+    }
 }
