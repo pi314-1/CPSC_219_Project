2ee5b0c27af4cba5fe38940e34556c3b6092e732
pi314-1 <95115278+pi314-1@users.noreply.github.com>
2021-12-13 21:04:06 -0700

    added main method and a controller for the view

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -13,13 +14,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			TabPane root = new TabPane();
 			
 			FXMLLoader loader = new FXMLLoader();
-			root = (BorderPane)loader.load(new 
-					FileInputStream("src/application/CalendarInterface.fxml"));
+			root = (TabPane)loader.load(new 
+					FileInputStream("src/view/CalendarInterface.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
